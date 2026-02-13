5200587fd747e2a74b90c2a532fade18c1537cf5
Parth Tolani <157131526+parthtolani@users.noreply.github.com>
2026-02-14 01:00:57 +0530

    Update special.js

--- special.js
+++ special.js
@@ -76,37 +76,37 @@ function start() {
 
 const pages = [
     `Dear Raja,\n Today is quite special, isn't it?`,
-    `The story goes 8 years back now...,\nIndeed a long time, eh?`,
-    `I don't know how god put that,\nkind of strength in me(in 8th std.) to ask you out that day,`,
+    `Our story goes 8 years back now...,\nIndeed a long time, eh?`,
+    `I don't know how god put that kind of strength in me(8th std.) to ask you out that day,`,
     `But tbh, I'm glad that he did, because I can't imagine my life without you now.`,
     `Let's walk through the memories, the laughter, the tears, and the love we've shared over these years.`,
-    `After 8th std, we became closest of friends, sharing everything,\n laughing together on Somanka and pradeep Sir, if you remember😂....`,
-    `And then, again I proposed you in 9th Std, \nif you remember, and if u don't, \nI had a slip of tongue saying that on PTM😝,`,
+    `After 8th, we became closest of friends, sharing everything,\n laughing together on Somanka and pradeep Sir😂....`,
+    `And then,I proposed you again in 9th Std, \nif you remember, and if u don't, \nI had a slip of tongue saying that on PTM😝,`,
     `I said \"I lob you\", and then you corrected saying,\n \"Thoda dhang se hi bol deta🤣\"`,
     `Although, I was quite happy, kyuki uska reply \n \"I love you toooo...but there are some conditions\" aaya tha😅`,
     `But I forgot the date, do u remember it? It was probably in sept or oct,perhaps`,
-    `Anyways, the conditions were, \"We\'re to young for this and blah....blah...blah...`,
+    `Anyways, the conditions were, \"We\'re tooo young for this and blah....blah...blah...`,
     `I guess we had quite innocence in our love,\n ofcourse due to being kids, but it was pure and real.`,
     `Then, nothing much happened, we continued being best of friends,\n vo moti dhwani ki aur vishakha ki bitching karte the😂,`,
-    `French class mein bahot bakchodi bhi kari, and bahottt kuchhh...`,
+    `French tuition mein bahot bakchodi bhi kari, and bahottt kuchhh...`,
     `Then straight after 10th std, on a random day, you called me in the evening, and said \"YESSS!\"`,
-    `That was like the most happiest moment of my life, I was on cloud nine, and I still am, knowing that you said yes to me`,
-    `Yyyyyyrrrrrr VNnnnn!....Mai vo date bhi bhul gya😅,\ntujhe yaad hai? Around April or May , plss yaad ho toh bataiyo😅`,
+    `That was like the happiest moment of my life, I was on cloud nine, and I still am, knowing that you said yes to me`,
+    `Yrrrrrr Vnnnn!....Mai vo date bhi bhul gya😅,\ntujhe yaad hai? Around April or May, plss yaad ho toh bataiyo😅`,
     `Aur fir iss part ke baad thoda guilt aajata hai, kyuki maine bahhhooooottttttt badi galti kardi thi😭`,
-    `I am reallllyyyyyyy Soooooorrrrrrrrrryyyyyyyyyy for that Raja, I know I hurted you deeply`,
+    `I am reallllyyyyyyy Soooooorrrrrrrrrryyyyyyyyyy for that Raja, I know I hurt you deeply`,
     `Till date, I regret that vnnnn! I wish I could undo that, but it happened, and now there's no going back!`,
     `But..But...But...\n since you're such a pretty soul, you forgave me, and we started it all over again, I'm so grateful for that.`,
     `You're the most adorable, kind-hearted, and loving person I have ever met.`,
     `Always the helpful one, always the one who cares, always the one who loves.`,
-    `Ha bhale hi phone dhange se use nhi karti, but jaisi bhi hai, perfect hai!`,
-    `It's tough to find someone like you in this world, raja. Grateful to have you in my life.`,
+    `Ha bhale hi phone dhang se use nhi karti🙄, baat saamne se initiate karne ka kasht nhi uthati😜, but jaisi bhi hai, perfect hai!`,
+    `It's tough to find someone like you in this world, raja. Grateful to have you in my life😊.`,
     `I know I'm not the best at expressing my feelings, but I hope this letter can convey even a fraction of how much you mean to me.`,
     `But, today's a day to celebrate love, and I wanted to celebrate our journey.`,
-    `It's a special day for us and I wanted to do something special, so I made this letter for you,`,
+    `It's a special day for us, and I wanted to do something special, so I made this letter for you,`,
     `I hope you like it, and I hope it brings a smile to your face, just like you always do to mine.`,
     `I love you, Raja. Happy Valentine's! ❤️ \n~ Your "Someone Special"`,
     `OOOPPPPPPS!\n Suspennnnseeeeeee to reveal nhi kiya😶‍🌫️!`,
-    `I'm coming Ahmedabad on your Birthday Raja and I can't wait to see you! 🥳🎉`,
+    `I'm coming to Ahmedabad on your Birthday Raja ,and I can't wait to see you! 🥳🎉`,
     `So, this is the end of the letter, but not the end of our story. We have many more chapters to write together, many more memories to create, and many more moments to cherish.`,
     `[[LIVE_COUNTDOWN]]`,
 ];
@@ -165,7 +165,7 @@ function typeText() {
     } else {
         typing = false;
         if (current < pages.length - 1) {
-            autoTransitionTimeout = setTimeout(nextPage, 4500); 
+            autoTransitionTimeout = setTimeout(nextPage, 4000); 
         }
     }
 }
@@ -210,3 +210,4 @@ window.addEventListener('resize', () => {
     canvas.width = window.innerWidth; 
     canvas.height = window.innerHeight; 
 });
+
